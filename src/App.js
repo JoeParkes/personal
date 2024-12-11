5895f1eeec3c3f4010381bb7895f52b9b5b83e76
Joe Parkes <joeparkes@hotmail.com>
2024-12-11 23:48:52 +0000

    feat: hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Jobs from "./components/jobs";
 import Skills from "./components/skills";
@@ -7,7 +7,7 @@ import Blob from "./components/blob";
 import ComingSoon from "./components/coming-soon";
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App max-w-[1440px] mx-auto p-6 md:p-12">
         <Blob />
 
@@ -24,7 +24,7 @@ function App() {
           <Route index element={<ComingSoon />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
